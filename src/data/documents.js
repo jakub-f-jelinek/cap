df2734314db8f785f8cbade8fed91a12a861d2da
Jakub Jelínek <jakub.f.jelinek@gmail.com>
2026-09-23 08:52:34 +0200

    New document added

--- src/data/documents.js
+++ src/data/documents.js
@@ -1,8 +1,13 @@
 import doc1 from "@/assets/docs/CAP_NORMALNI_RIZENI_ZABIJI_TISKOVA_Konference.pdf";
+import doc2 from "@/assets/docs/TZ_Normalni_rizeni_zabiji.pdf";
 
 export const documents = [
   {
     href: doc1,
     title: "ČAP NORMALNÍ ŘÍZENÍ ZABÍJÍ - tisková konference",
+  },
+  {
+    href: doc2,
+    title: "Tisková zpráva - Normální řízení zabíjí",
   },
 ];
